6fbc707f5afc913cf444a47065e51ca38e887e05
maugdaug <amauger87@gmail.com>
2018-07-17 22:22:35 -0700

    added more to IK_debug

--- IK_debug.py
+++ IK_debug.py
@@ -73,13 +73,13 @@ def test_code(test_case):
 	# Create Modified DH parameters
     # Define Modified DH Transformation matrix
 
-	s = {alpha0: 0, a0: 0, d1: 0.75, q1: q1,
-        alpha1: -pi/2,	a1: 0.35,	d2: 0,		q2:	q2-pi/2,
-		alpha2:   0,	a2: 1.25,	d3: 0,		q3:	q3,
-		alpha3: -pi/2,	a3: -0.054,	d4: 1.5,	q4:	q4,
-		alpha4: pi/2,	a4: 0,		d5: 0,		q5:	q5,
-		alpha5: -pi/2,	a5: 0,		d6: 0,		q6:	q6,
-		alpha6:   0,	a6: 0,		d7: 0.303,	q7:	q7} # gripper
+	s = {alpha0: 0,	a0: 0,		d1: 0.75,	q1: q1,
+	alpha1: -pi/2,	a1: 0.35,	d2: 0,		q2: q2-pi/2,
+	alpha2:   0,	a2: 1.25,	d3: 0,		q3: q3,
+	alpha3: -pi/2,	a3: -0.054,	d4: 1.5,	q4: q4,
+	alpha4: pi/2,	a4: 0,		d5: 0,		q5: q5,
+	alpha5: -pi/2,	a5: 0,		d6: 0,		q6: q6,
+	alpha6:   0,	a6: 0,		d7: 0.303,	q7: q7} # gripper
 
 
 
